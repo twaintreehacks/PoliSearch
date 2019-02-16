ae22465aa2f93f857e86cb273f28e47e16382994
Alex Donovan <aldonovan@micds.org>
2019-02-16 10:55:09 -0800

    Added HTML Template for Homepage

--- PoliSearch/PolitiSearch/views.py
+++ PoliSearch/PolitiSearch/views.py
@@ -2,5 +2,5 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
-def index(request):
-    return HttpResponse("Hello, world!")
+def home(request):
+    return render(request, 'PolitiSearch/home.html')
