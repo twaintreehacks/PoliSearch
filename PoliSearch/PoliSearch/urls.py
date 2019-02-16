63a62487b281a4ea763b278b990bd444895d5c90
Alex Donovan <aldonovan@micds.org>
2019-02-16 12:29:24 -0800

    Small fix to navbar

--- PoliSearch/PoliSearch/urls.py
+++ PoliSearch/PoliSearch/urls.py
@@ -18,5 +18,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('', include('PolitiSearch.urls')),
+    #path('about/', include('PolitiSearch.urls')),
     path('admin/', admin.site.urls),
 ]
